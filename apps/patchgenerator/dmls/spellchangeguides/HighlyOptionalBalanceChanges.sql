64d4ccf18b33beaa02c17a15701fdd9651fae5ba
matthewjmeidt <matthewjmeidt@gmail.com>
2026-01-26 23:29:13 -0600

    - Talent update guide tweaks - Added quest only spells to the trainers (missed maul)

--- apps/patchgenerator/dmls/spellchangeguides/HighlyOptionalBalanceChanges.sql
+++ apps/patchgenerator/dmls/spellchangeguides/HighlyOptionalBalanceChanges.sql
@@ -5,3 +5,4 @@
 select Spell.name_lang_en_gb,effect_bonus_coefficient_0 from spell where id in (467,782,1075,8914,9756,9910,26992,53307);
 select Spell.name_lang_en_gb,effect_bonus_coefficient_0 from spell where id in (7294, 10298, 10299, 10300, 10301, 27150, 54043);
 update spell set effect_bonus_coefficient_0 = 0.033 where id in (467,782,1075,8914,9756,9910,26992,53307);
+
